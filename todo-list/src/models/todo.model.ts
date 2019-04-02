7ae68a3de10998624f64363948927de7e9323b41
= <jt.ascend@gmail.com>
2019-04-02 14:31:44 +1000

    lb4 gen app and first model

---
+++ todo-list/src/models/todo.model.ts
@@ -0,0 +1,32 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Todo extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  title: string;
+
+  @property({
+    type: 'string',
+  })
+  desc?: string;
+
+  @property({
+    type: 'boolean',
+    default: N,
+  })
+  isComplete?: boolean;
+
+
+  constructor(data?: Partial<Todo>) {
+    super(data);
+  }
+}
